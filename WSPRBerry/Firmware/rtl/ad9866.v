c5979c42cc03e813d6d37c431250c7e9904120f9
pa3gsb <pa3gsb@gmail.com>
2018-04-19 20:42:14 +0200

    improvement init ad9866

--- WSPRBerry/Firmware/rtl/ad9866.v
+++ WSPRBerry/Firmware/rtl/ad9866.v
@@ -41,9 +41,9 @@ parameter bit [0:19][8:0] initarray = {
     {1'b0,8'h00}, // Address 0x01,
     {1'b0,8'h00}, // Address 0x02, 
     {1'b0,8'h00}, // Address 0x03, 
-    {1'b1,8'h02}, // Address 0x04, 
+    {1'b1,8'h01}, // Address 0x04, 
     {1'b0,8'h00}, // Address 0x05, 
-    {1'b1,8'h14}, // Address 0x06,
+    {1'b1,8'h10}, // Address 0x06,
     {1'b1,8'h21}, // Address 0x07, Initiate DC offset calibration and RX filter on
     {1'b1,8'h4b}, // Address 0x08, RX filter f-3db at ~34 MHz after scaling
     {1'b1,8'h7F}, // Address 0x09, 
