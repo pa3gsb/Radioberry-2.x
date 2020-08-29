2e50e1bf71162940da8540e1f8eb617505bb05f2
pa3gsb <pa3gsb@gmail.com>
2020-08-29 17:05:08 +0200

    soapy en soapy radioberry; script install added

---
+++ SBC/rpi-4/device_driver/scripts/soapy_install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+echo "Installation of radioberry SOAPY SDR setup"
+echo ""
+echo "You need to install [SoapySDR/SoapyRadioberrySDR/reboot] ?"
+
+read input
+
+if [[ $input == "SoapySDR" ]]; then
+	echo "Installing SoapySDR..."
+	
+	function install_dependency {
+		echo "--- Installing dependency: $1"
+		sudo apt-get -y install $1
+	}
+
+	install_dependency git
+	install_dependency cmake
+	
+	cd /tmp
+	git clone https://github.com/pothosware/SoapySDR.git
+	cd SoapySDR
+
+	mkdir build
+	cd build
+	cmake ..
+	make -j4
+	sudo make install
+	sudo ldconfig
+	cd ../..
+	rm -rf SoapySDR
+	
+	SoapySDRUtil --info
+	
+	echo ""
+	echo "SoapySDR  installed."
+	
+elif [[ $input == "SoapyRadioberrySDR" ]]; then
+	echo "Installing SoapyRadioberrySDR ..."
+	
+	function install_dependency {
+		echo "--- Installing dependency: $1"
+		sudo apt-get -y install $1
+	}
+
+	install_dependency git
+	install_dependency cmake
+	
+	cd /tmp
+	git clone --depth=1 https://github.com/pa3gsb/Radioberry-2.x.git
+	cd Radioberry-2.x/SBC/rpi-4/SoapyRadioberrySDR
+	mkdir build
+	cd build
+	cmake ..
+	make -j4
+	sudo make install
+	sudo ldconfig
+	cd ../..
+	rm -rf Radioberry-2.x
+
+	SoapySDRUtil --probe="driver=radioberry"
+
+	echo ""
+	echo "SoapyRadioberrySDR installed."
+
+elif [[ $input == "reboot" ]]; then	
+
+	sudo reboot
+
+else 
+	echo "You did not make a selection; Nothing installed!"
+	echo "73!"
+fi
+
+
