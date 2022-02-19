8fa3d214a198f48b45185083d3e6680067e14c3a
paulh002 <paul@Paul01>
2022-02-19 02:18:50 +0100

    update transmit

--- SBC/rpi-4/SoapyRadioberrySDR/SoapyRadioberryStreaming.cpp
+++ SBC/rpi-4/SoapyRadioberrySDR/SoapyRadioberryStreaming.cpp
@@ -222,8 +222,8 @@ int SoapyRadioberry::writeStream(SoapySDR::Stream *stream, const void * const *b
 			
 			tx.i8TxBuffer[0] = (unsigned char)((itarget_buffer[j] & 0xff00) >> 8);
 			tx.i8TxBuffer[1] = (unsigned char)(itarget_buffer[j] & 0xff);
-			tx.i8TxBuffer[2] = (unsigned char)((itarget_buffer[j + 1] & 0xff00) >> 8);
-			tx.i8TxBuffer[3] = (unsigned char)(itarget_buffer[j + 1] & 0xff); 
+			tx.i8TxBuffer[2] = (unsigned char)(((-1 * itarget_buffer[j + 1]) & 0xff00) >> 8);
+			tx.i8TxBuffer[3] = (unsigned char)(( -1 * itarget_buffer[j + 1]) & 0xff); 
 				
 			ret = write(fd_rb, &tx, sizeof(uint32_t));	
 			j += 2;
