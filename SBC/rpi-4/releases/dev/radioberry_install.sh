ce3f167782f99294f9153bcb9e05ad17c1607daa
pa3gsb <pa3gsb@gmail.com>
2023-10-25 12:35:37 +0200

    install script improvement

--- SBC/rpi-4/releases/dev/radioberry_install.sh
+++ SBC/rpi-4/releases/dev/radioberry_install.sh
@@ -7,9 +7,9 @@ echo "Radioberry software installation."
 echo ""
 echo "You will install the following versions: "
 echo ""
-echo "	Gateware version 73.2"
-echo "	Driver version 0.92"
-echo "	Firmware version 2021.09.10"
+echo "	Gateware version 73.3"
+echo "	Driver version 0.94"
+echo "	Firmware version 2022.01.24"
 echo "============================================"
 echo ""
 echo ""
@@ -79,37 +79,59 @@ fi
 	
 cd Radioberry-2.x/SBC/rpi-4/device_driver/driver
 make
-sudo cp radioberry.ko /lib/modules/$(uname -r)/kernel/drivers/sdr
+if [ $? -eq 0 ]; then
+	sudo cp radioberry.ko /lib/modules/$(uname -r)/kernel/drivers/sdr
 
-sudo dtc -@ -I dts -O dtb -o radioberry.dtbo radioberry.dts
-sudo cp radioberry.dtbo /boot/overlays
-#add driver to config.txt
-sudo grep -Fxq "dtoverlay=radioberry" /boot/config.txt || sudo sed -i '$ a dtoverlay=radioberry' /boot/config.txt
+	sudo dtc -@ -I dts -O dtb -o radioberry.dtbo radioberry.dts
+	sudo cp radioberry.dtbo /boot/overlays
+	#add driver to config.txt
+	sudo grep -Fxq "dtoverlay=radioberry" /boot/config.txt || sudo sed -i '$ a dtoverlay=radioberry' /boot/config.txt
 
-cd ../../../../..
+	cd ../../../../..
+		
+	sudo depmod	
+	#register radioberry driver
+	sudo modprobe radioberry
+	sudo chmod 666 /dev/radioberry
+	#show radioberry driver info.
+	sudo modinfo radioberry
+
+	echo ""
+	echo "Radioberry driver installed."
+else
+    echo "Radioberry driver installation failed."
+    echo ""
+	echo "You are using a linux version without linux header files; choose an other distro."
 	
-sudo depmod	
-#register radioberry driver
-sudo modprobe radioberry
-sudo chmod 666 /dev/radioberry
-#show radioberry driver info.
-sudo modinfo radioberry
+	sudo rm -rf Radioberry-2.x
+	
+	exit 1
+fi
 
-echo ""
-echo "Radioberry driver installed."
 
 #-----------------------------------------------------------------------------
 echo "Installing Radioberry firmware..."
 	
 cd Radioberry-2.x/SBC/rpi-4/device_driver/firmware
 sudo make
-sudo cp radioberry  /usr/local/bin
-sudo chmod +x /usr/local/bin/radioberry
+	if [ $? -eq 0 ]; then
 
-cd ../../../../..
+	sudo cp radioberry  /usr/local/bin
+	sudo chmod +x /usr/local/bin/radioberry
 
-echo ""
-echo "Radioberry firmware installed."	
+	cd ../../../../..
+
+	echo ""
+	echo "Radioberry firmware installed."	
+else
+    echo "Radioberry firmware installation failed."
+    echo ""
+	echo "Looking into the log and try to find out what is wrong."
+	
+	sudo rm -rf Radioberry-2.x
+	
+	exit 1
+fi
 
 #-----------------------------------------------------------------------------
 echo "Installing radioberry service ..."
@@ -142,4 +164,4 @@ echo "============================================"
 echo ""
 echo ""
 
-
+exit 0
